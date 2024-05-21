d19f33f705e1b0c281f8bddb579c45e06b3c3bfe
islude8 <odair181818@gmail.com>
2024-05-21 19:58:18 -0400

    Adicionado variaveis para serem acessadas pelo escopo.

---
+++ ContaBanco/src/ContaBanco.java
@@ -0,0 +1,14 @@
+
+
+public class ContaBanco {
+    
+        int agenciaBancaria = 1021;
+        String contaBancaria = "067-8";
+        String nomeDoCliente = "Mario Andrade";
+        long saldoBancario = 273.48;
+    public static void main(String[] args) throws Exception {
+        
+        
+
+    }
+}
